fb919b97115dfd6c612cac283e92eb90310d7073
Flavio <flavio_125@hotmail.com>
2022-10-24 16:36:15 -0300

    Componentização de Widgets

--- lib/app_widget.dart
+++ lib/app_widget.dart
@@ -1,8 +1,11 @@
+// ignore_for_file: prefer_const_constructors, use_key_in_widget_constructors
+
 import 'package:flutter/material.dart';
 import 'package:ola_flavio/app_controller.dart';
 import 'package:ola_flavio/home_page.dart';
 
 class AppWidget extends StatelessWidget {
+  
   @override
   Widget build(BuildContext context) {
     return AnimatedBuilder(
--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,7 +1,10 @@
+// ignore_for_file: prefer_const_literals_to_create_immutables, prefer_const_constructors, use_key_in_widget_constructors
+
 import 'package:flutter/material.dart';
 import 'package:ola_flavio/app_controller.dart';
 
 class HomePage extends StatefulWidget {
+  
   @override
   State<HomePage> createState() {
     return HomePageState();
@@ -15,26 +18,39 @@ class HomePageState extends State<HomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text(
+        title: Text(
           'Home Page',
         ),
+        actions: [
+          CustomSwitch(),
+        ],        
       ),
       body: Center(
-        child: Switch(
-          value: AppController.instance.isDarkTheme,
-          onChanged: (value) {
-            AppController.instance.changeTheme();
-          },
-        ),
+        child: CustomSwitch(),
       ),
       floatingActionButton: FloatingActionButton(
-        child: const Icon(Icons.add),
+        child: Icon(Icons.add),
         onPressed: () {
           setState(() {
             counter++;
           });
         },
       ),
+    );
+  }
+}
+
+class CustomSwitch extends StatelessWidget {
+  // ignore: prefer_const_constructors_in_immutables
+  CustomSwitch({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Switch(
+      value: AppController.instance.isDarkTheme,
+      onChanged: (value) {
+        AppController.instance.changeTheme();
+      },
     );
   }
 }
