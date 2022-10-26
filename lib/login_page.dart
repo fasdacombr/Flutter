912c71ef51cbd7049e4f81d5588fd9b5af3aa264
Flavio <flavio_125@hotmail.com>
2022-10-25 21:24:22 -0300

    Stack

--- lib/login_page.dart
+++ lib/login_page.dart
@@ -14,64 +14,81 @@ class _LoginPageState extends State<LoginPage> {
   String email = '';
   String password = '';
 
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: SingleChildScrollView(
-        child: SizedBox(
-          width: MediaQuery.of(context).size.width,
-          height: MediaQuery.of(context).size.height,
-          child: Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              // ignore: prefer_const_literals_to_create_immutables
-              children: [
-                Container(
-                  width: 200,
-                  height: 200,
-                  child: Image.asset(
-                    'assets/images/logo.png',
+  Widget _body() {
+    return Column(
+      children: [
+        SingleChildScrollView(
+          child: SizedBox(
+            width: MediaQuery.of(context).size.width,
+            height: MediaQuery.of(context).size.height,
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                // ignore: prefer_const_literals_to_create_immutables
+                children: [
+                  Container(
+                    width: 200,
+                    height: 200,
+                    child: Image.asset(
+                      'assets/images/logo.png',
+                    ),
                   ),
-                ),
-                Container(height: 20),
-                TextField(
-                  onChanged: (text) {
-                    email = text;
-                  },
-                  keyboardType: TextInputType.emailAddress,
-                  decoration: InputDecoration(
-                    labelText: 'Email',
-                    border: OutlineInputBorder(),
+                  Container(height: 20),
+                  TextField(
+                    onChanged: (text) {
+                      email = text;
+                    },
+                    keyboardType: TextInputType.emailAddress,
+                    decoration: InputDecoration(
+                      labelText: 'Email',
+                      border: OutlineInputBorder(),
+                    ),
                   ),
-                ),
-                SizedBox(height: 10),
-                TextField(
-                  onChanged: (text) {
-                    password = text;
-                  },
-                  obscureText: true,
-                  decoration: InputDecoration(
-                    labelText: 'Password',
-                    border: OutlineInputBorder(),
+                  SizedBox(height: 10),
+                  TextField(
+                    onChanged: (text) {
+                      password = text;
+                    },
+                    obscureText: true,
+                    decoration: InputDecoration(
+                      labelText: 'Password',
+                      border: OutlineInputBorder(),
+                    ),
                   ),
-                ),
-                SizedBox(height: 15),
-                ElevatedButton(
-                  onPressed: () {
-                    if (email == 'flavio@fasda.com.br' && password == '321') {
-                      Navigator.of(context).pushReplacementNamed('/home');
-                    } else {
-                      print('Login inválido!');
-                    }
-                  },
-                  child: Text('Entrar'),
-                ),
-              ],
+                  SizedBox(height: 15),
+                  ElevatedButton(
+                    onPressed: () {
+                      if (email == 'flavio@fasda.com.br' && password == '321') {
+                        Navigator.of(context).pushReplacementNamed('/home');
+                      } else {
+                        print('Login inválido!');
+                      }
+                    },
+                    child: Text('Entrar'),
+                  ),
+                ],
+              ),
             ),
           ),
         ),
-      ),
+      ],
     );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: Stack(
+      children: [
+        SizedBox(
+          height: MediaQuery.of(context).size.height,
+          child: Image.asset('assets/images/background_1.jpg', fit: BoxFit.cover,)),
+        Container(
+          color: Colors.black.withOpacity(0.3),
+        ),
+        _body(),
+      ],
+    ));
   }
 }
