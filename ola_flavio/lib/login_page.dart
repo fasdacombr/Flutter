324b0f4cc9db6fbe414a436ec33a527fa391f088
fasdacombr <flavio_125@hotmail.com>
2022-11-02 09:13:05 -0300

    Corrigido erro de rolagem ao mostrar teclado

--- ola_flavio/lib/login_page.dart
+++ ola_flavio/lib/login_page.dart
@@ -1,6 +1,7 @@
 // ignore_for_file: prefer_const_constructors
 
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 import 'package:ola_flavio/home_page.dart';
 
 class LoginPage extends StatefulWidget {
@@ -15,83 +16,85 @@ class _LoginPageState extends State<LoginPage> {
   String password = '';
 
   Widget _body() {
-    return Column(
-      children: [
-        SingleChildScrollView(
-          child: SizedBox(
-            width: MediaQuery.of(context).size.width,
-            height: MediaQuery.of(context).size.height,
-            child: Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                // ignore: prefer_const_literals_to_create_immutables
-                children: [
-                  Container(
-                    width: 200,
-                    height: 200,
-                    child: Image.asset(
-                      'assets/images/logo.png',
-                    ),
-                  ),
-                  Container(height: 20),
-                  Card(
-                    child: Padding(
-                      padding: const EdgeInsets.only(
-                          left: 12, right: 12, top: 20, bottom: 12),
-                      child: Column(
-                        children: [
-                          TextField(
-                            onChanged: (text) {
-                              email = text;
-                            },
-                            keyboardType: TextInputType.emailAddress,
-                            decoration: InputDecoration(
-                              labelText: 'Email',
-                              border: OutlineInputBorder(),
-                            ),
-                          ),
-                          SizedBox(height: 10),
-                          TextField(
-                            onChanged: (text) {
-                              password = text;
-                            },
-                            obscureText: true,
-                            decoration: InputDecoration(
-                              labelText: 'Password',
-                              border: OutlineInputBorder(),
-                            ),
-                          ),
-                          SizedBox(height: 15),
-                          ElevatedButton(
-                            style: ElevatedButton.styleFrom(
-                                backgroundColor: Colors.green),
-                            onPressed: () {
-                              if (email == 'flavio@fasda.com.br' &&
-                                  password == '321') {
-                                Navigator.of(context)
-                                    .pushReplacementNamed('/home');
-                              } else {
-                                print('Login inválido!');
-                              }
-                            },
-                            child: Container(
-                                width: double.infinity,
-                                child: Text(
-                                  'Entrar',
-                                  textAlign: TextAlign.center,
-                                )),
-                          ),
-                        ],
+    return SingleChildScrollView(
+      child: Column(
+        children: [
+          SingleChildScrollView(
+            child: SizedBox(
+              width: MediaQuery.of(context).size.width,
+              height: MediaQuery.of(context).size.height,
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  // ignore: prefer_const_literals_to_create_immutables
+                  children: [
+                    Container(
+                      width: 200,
+                      height: 200,
+                      child: Image.asset(
+                        'assets/images/logo.png',
                       ),
                     ),
-                  ),
-                ],
+                    Container(height: 20),
+                    Card(
+                      child: Padding(
+                        padding: const EdgeInsets.only(
+                            left: 12, right: 12, top: 20, bottom: 12),
+                        child: Column(
+                          children: [
+                            TextField(
+                              onChanged: (text) {
+                                email = text;
+                              },
+                              keyboardType: TextInputType.emailAddress,
+                              decoration: InputDecoration(
+                                labelText: 'Email',
+                                border: OutlineInputBorder(),
+                              ),
+                            ),
+                            SizedBox(height: 10),
+                            TextField(
+                              onChanged: (text) {
+                                password = text;
+                              },
+                              obscureText: true,
+                              decoration: InputDecoration(
+                                labelText: 'Password',
+                                border: OutlineInputBorder(),
+                              ),
+                            ),
+                            SizedBox(height: 15),
+                            ElevatedButton(
+                              style: ElevatedButton.styleFrom(
+                                  backgroundColor: Colors.green),
+                              onPressed: () {
+                                if (email == 'flavio@fasda.com.br' &&
+                                    password == '321') {
+                                  Navigator.of(context)
+                                      .pushReplacementNamed('/home');
+                                } else {
+                                  print('Login inválido!');
+                                }
+                              },
+                              child: Container(
+                                  width: double.infinity,
+                                  child: Text(
+                                    'Entrar',
+                                    textAlign: TextAlign.center,
+                                  )),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
               ),
             ),
           ),
-        ),
-      ],
+        ],
+      ),
     );
   }
 
