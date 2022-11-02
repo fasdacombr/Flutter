58727c6fc147fb22c0605e615c068c3d97f5d08b
Flavio <flavio_125@hotmail.com>
2022-11-02 19:40:52 -0300

    Componetizando a View

---
+++ conversor_curso/lib/app/components/currency_box.dart
@@ -0,0 +1,57 @@
+import 'package:flutter/material.dart';
+
+class CurrencyBox extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Expanded(
+          flex: 1,
+          child: SizedBox(
+            height: 57,
+            child: DropdownButton(
+              iconEnabledColor: Colors.amber,
+              isExpanded: true,
+              underline: Container(
+                height: 1,
+                color: Colors.amber,
+              ),
+              value: 0,
+              items: const [
+                DropdownMenuItem(
+                  value: 0,
+                  child: Text('Real'),
+                ),
+                DropdownMenuItem(
+                  child: Text('Dolar'),
+                ),
+                DropdownMenuItem(
+                  child: Text('Euro'),
+                ),
+                DropdownMenuItem(
+                  child: Text('Bitcoin'),
+                ),
+              ],
+              onChanged: (value) {},
+            ),
+          ),
+        ),
+        const SizedBox(width: 10),
+        const Expanded(
+          flex: 2,
+          child: TextField(
+            decoration: InputDecoration(
+              enabledBorder: UnderlineInputBorder(
+                borderSide: BorderSide(color: Colors.amber),
+              ),
+              focusedBorder: UnderlineInputBorder(
+                borderSide: BorderSide(color: Colors.amber),
+              ),
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
--- conversor_curso/lib/app/views/home_view.dart
+++ conversor_curso/lib/app/views/home_view.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import '../components/currency_box.dart';
+
 class HomeView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
@@ -9,7 +11,7 @@ class HomeView extends StatelessWidget {
         height: MediaQuery.of(context).size.height,
         child: Padding(
           padding:
-              const EdgeInsets.only(left: 20, right: 20, top: 100, bottom: 20),
+              const EdgeInsets.only(left: 30, right: 30, top: 100, bottom: 20),
           child: Column(
             children: [
               Image.asset(
@@ -17,61 +19,14 @@ class HomeView extends StatelessWidget {
                 width: 150,
                 height: 150,
               ),
-              SizedBox(
-                width: MediaQuery.of(context).size.width,
-                child: Row(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Expanded(
-                        flex: 1,
-                        child: SizedBox(
-                          height: 57,
-                          child: DropdownButton(
-                            isExpanded: true,
-                            underline: Container(
-                              height: 1,
-                              color: Colors.amber,
-                            ),
-                            value: 0,
-                            items: const [
-                              DropdownMenuItem(
-                                value: 0,
-                                child: Text('Real'),
-                              ),
-                              DropdownMenuItem(
-                                child: Text('Dolar'),
-                              ),
-                              DropdownMenuItem(
-                                child: Text('Euro'),
-                              ),
-                              DropdownMenuItem(
-                                child: Text('Bitcoin'),
-                              ),
-                            ],
-                            onChanged: (value) {},
-                          ),
-                        )),
-                    const SizedBox(width: 10),
-                    const Expanded(
-                      flex: 2,
-                      child: TextField(
-                        decoration: InputDecoration(
-                          enabledBorder: UnderlineInputBorder(
-                            borderSide: BorderSide(color: Colors.amber),
-                          ),
-                          focusedBorder: UnderlineInputBorder(
-                            borderSide: BorderSide(color: Colors.amber),
-                          ),
-                        ),
-                      ),
-                    ),
-                  ],
-                ),
-              ),
+              SizedBox(height: 50),
+              CurrencyBox(),
+              SizedBox(height: 10),
+              CurrencyBox(),
               const SizedBox(height: 50),
               ElevatedButton(
                 style: ElevatedButton.styleFrom(
-                  primary: Colors.amber,
+                  backgroundColor: Colors.amber,
                 ),
                 onPressed: () {},
                 child: const Text('CONVERTER'),
