cae72fc89a5c08bf95aa180db0e565a4380c5f3d
fasdacombr <flavio_125@hotmail.com>
2022-11-13 12:24:12 -0300

    (API) Refatoração, com bug no test...

--- todo_list/lib/src/repositories/todo_repository.dart
+++ todo_list/lib/src/repositories/todo_repository.dart
@@ -3,23 +3,19 @@ import 'package:dio/dio.dart';
 
 import 'package:todo_list/src/models/todo_model.dart';
 
-
 class TodoRepository {
-  Dio? dio;
+  //Dio? dio;
+  var dio = Dio();
   final url = 'https://jsonplaceholder.typicode.com/todos';
 
-  TodoRepository({Dio? client}){
-    if (client == null) {
-      this.dio = Dio();
-    } else {
-      this.dio = client;
-    }
-  }
+  TodoRepository({Dio? client}) : dio = client ?? Dio();
 
   Future<List<TodoModel>> fetchTodos() async {
-    final response = await dio?.get(url);
-    final list = response?.data as List;
+    final response = await dio.get(url);
+    final list = response.data as List;
+    return list.map((json) => TodoModel.fromJson(json)).toList();
 
+/*
     List<TodoModel> todos = [];
     for (var json in list) {
       final todo = TodoModel.fromJson(json);
@@ -27,5 +23,6 @@ class TodoRepository {
     }
 
     return todos;
+*/
   }
 }
--- todo_list/test/src/repositories/todo_repository_test.dart
+++ todo_list/test/src/repositories/todo_repository_test.dart
@@ -1,18 +1,96 @@
+import 'dart:convert';
+
 import 'package:dio/dio.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:mockito/annotations.dart';
 import 'package:mockito/mockito.dart';
 import 'package:todo_list/src/repositories/todo_repository.dart';
 
-class DioMock extends Mock implements Dio {
+class DioMock extends Mock implements Dio {}
 
-}
-
-main() {
+@GenerateMocks([Dio])
+void main() {
   final dio = DioMock();
   final repository = TodoRepository(client: dio);
 
   test("Deve trazer uma lista de TodoModel", () async {
-     final list = await repository.fetchTodos();
-     expect(list[4].title, 'laboriosam mollitia et enim quasi adipisci quia provident illum');
+    when(dio.get(jsonData)).thenAnswer(
+      (inv) async => Future.value(Response(
+        statusCode: 200,
+        data: jsonDecode(jsonData),
+        requestOptions: RequestOptions(path: jsonData),
+      )),
+    );
+
+    final list = await repository.fetchTodos();
+    expect(list[4].title,
+        'laboriosam mollitia et enim quasi adipisci quia provident illum');
   });
 }
+
+String jsonData = ''' 
+
+[
+  {
+    "userId": 1,
+    "id": 1,
+    "title": "delectus aut autem",
+    "completed": false
+  },
+  {
+    "userId": 1,
+    "id": 2,
+    "title": "quis ut nam facilis et officia qui",
+    "completed": false
+  },
+  {
+    "userId": 1,
+    "id": 3,
+    "title": "fugiat veniam minus",
+    "completed": false
+  },
+  {
+    "userId": 1,
+    "id": 4,
+    "title": "et porro tempora",
+    "completed": true
+  },
+  {
+    "userId": 1,
+    "id": 5,
+    "title": "laboriosam mollitia et enim quasi adipisci quia provident illum",
+    "completed": false
+  },
+  {
+    "userId": 1,
+    "id": 6,
+    "title": "qui ullam ratione quibusdam voluptatem quia omnis",
+    "completed": false
+  },
+  {
+    "userId": 1,
+    "id": 7,
+    "title": "illo expedita consequatur quia in",
+    "completed": false
+  },
+  {
+    "userId": 1,
+    "id": 8,
+    "title": "quo adipisci enim quam ut ab",
+    "completed": true
+  },
+  {
+    "userId": 1,
+    "id": 9,
+    "title": "molestiae perspiciatis ipsa",
+    "completed": false
+  },
+  {
+    "userId": 1,
+    "id": 10,
+    "title": "illo est ratione doloremque quia maiores aut",
+    "completed": true
+  }
+]
+
+''';
